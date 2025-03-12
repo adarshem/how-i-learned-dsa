f644227ff290199516e9831cec8fd3a270cc55cf
adarshem <adarshem@gmail.com>
2025-03-11 20:04:46 -0700

    Fixed move zeroes function

--- src/Data-Structures/arrays/moveZeroes.ts
+++ src/Data-Structures/arrays/moveZeroes.ts
@@ -1,13 +1,16 @@
-function moveZeroes(nums: number[]): number[] {
-  let i = 0;
-  for (let j = 1; j < nums.length; j++) {
-    if (nums[j] === 0) {
-      [nums[i], nums[j]] = [nums[j], nums[i]];
-      i++;
+function moveZeroes(nums: number[]): void {
+  let lastNonZeroFoundAt = 0;
+
+  // Iterate through the array
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      // Swap elements if the current element is non-zero
+      [nums[lastNonZeroFoundAt], nums[i]] = [nums[i], nums[lastNonZeroFoundAt]];
+      lastNonZeroFoundAt++;
     }
   }
-
-  return nums;
 }
 
-console.log(moveZeroes([0, 1, 2, 0, 3, 12, 0])); // Output: [1, 3, 12, 0, 0]
+const nums = [0, 1, 2, 0, 3, 12, 0];
+moveZeroes(nums);
+console.log(nums); // Output: [1, 2, 3, 12, 0, 0, 0]
